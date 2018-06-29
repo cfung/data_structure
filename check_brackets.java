892a309f3a6949b65c25458fd5e7640a943b60df
Chester Fung <cfung@netflix.com>
2018-06-29 14:13:01 -0700

    init

---
+++ check_brackets.java
@@ -0,0 +1,63 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.Stack;
+
+class Bracket {
+    Bracket(char type, int position) {
+        this.type = type;
+        this.position = position;
+    }
+
+    boolean Match(char c) {
+        if (this.type == '[' && c == ']')
+            return true;
+        if (this.type == '{' && c == '}')
+            return true;
+        if (this.type == '(' && c == ')')
+            return true;
+        return false;
+    }
+
+    char type;
+    int position;
+}
+
+class check_brackets {
+    public static void main(String[] args) throws IOException {
+        InputStreamReader input_stream = new InputStreamReader(System.in);
+        BufferedReader reader = new BufferedReader(input_stream);
+        String text = reader.readLine();
+
+        Boolean result = true;
+
+        Stack<Bracket> opening_brackets_stack = new Stack<Bracket>();
+        for (int position = 0; position < text.length(); ++position) {
+            char next = text.charAt(position);
+
+            if (next == '(' || next == '[' || next == '{') {
+                // Process opening bracket, write your code here
+                Bracket bracket = new Bracket(next, position);
+                //bracket.type = next;
+                opening_brackets_stack.push(bracket);
+            }
+
+            if (next == ')' || next == ']' || next == '}') {
+                // Process closing bracket, write your code here
+
+                if (opening_brackets_stack.empty()){
+                    result = false;
+                }
+                Bracket top;
+                top = opening_brackets_stack.pop();
+
+                if ((top.type == '(' && next != ')') || (top.type == '[' && next != ']') || (top.type == '{' && next != '}')){
+                    result = false;
+                }
+            }
+        }
+
+        // Printing answer, write your code here
+        System.out.println(result);
+    }
+}

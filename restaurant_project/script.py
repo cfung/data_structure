1172c1bbdc210ecc017b429e6e7eadbd84f8f056
sk <chesterfung@gmail.com>
2018-12-08 09:37:44 -0800

    init project

---
+++ restaurant_project/script.py
@@ -0,0 +1,15 @@
+'''
+All of your code (unless you choose the Trie implementation mentioned below) will be written in script.py.
+
+The data is stored in data.py and imported into script.py so you can properly access the data. You also have access to various data structures. If these data structures don't fit your needs for the project, feel free to make up your own variations! We just ask that you do NOT utilize Python's built-in list class to handle your data.
+
+This project consists of main two parts:
+
+Implementing an autocomplete for food types that returns a list of possible food types based on the beginning of a word. You'll use the data stored in food_types, which is a list of all the different types of food the user can select from. It is up to you how to properly store and retrieve the data. Some possible options are listed under the Part 1 section.
+Retrieving and displaying all of the restaurant data. This data is stored in restaurant_data, which is currently a list of lists. The first value in each list is the food type, the second value is the restaurant name, the third value is the price, the fourth value is the rating, and the fifth value is the address. It is up to you how to properly store and retrieve the data. Some possible options are listed under the Part 2 section.
+'''
+
+
+
+
+

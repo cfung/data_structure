d7afca7b2b80f14bdab8eb1d5ab06e1e197e31e6
sk <chesterfung@gmail.com>
2018-12-10 23:09:45 -0800

    update food_type

---
+++ restaurant_project/restaurant_lib.py
@@ -0,0 +1,19 @@
+'''
+The first value in each list is the food type, 
+the second value is the restaurant name, 
+the third value is the price, 
+the fourth value is the rating, and the 
+fifth value is the address
+
+Part 1
+a. linkedlist:  "german" -> "japanese" -> "vegetarian" -> "french"
+b. Hash Map: "c":  "czech" -> "chinese" -> "cafe"
+             "i":  "indian" -> "italian"
+
+Part 2
+i.e. linked list of linked lists
+["german", "name", "price", "rating", "address"]
+["italisn", "name", "price", "rating", "address"]
+'''
+food_type = ['german', 'japanese', 'vegetarian', 'french', 'czech', 'chinese', 'cafe', 'indian', 'italian']
+restaurant_data = [['begonia', 'cautiousness']]
